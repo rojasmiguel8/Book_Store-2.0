68487fddb3b383d45495d5b9adee28dcdbec012e
rojasmiguel8 <rojasmiguelangel08@gmail.com>
2024-09-14 17:53:05 -0400

    search2.0

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 
 # Create your views here.
@@ -19,4 +20,12 @@ class BookDetailView(LoginRequiredMixin, DetailView):
 class SearchResultsView(ListView):
      model = Book
      template_name = 'books/search.html'
+     
+
+     def get_queryset(self):
+          query = self.request.GET.get("q")
+          return Book.objects.filter(
+          Q(title__icontains=query) | Q(author__icontains=query)
+          )
+
      
